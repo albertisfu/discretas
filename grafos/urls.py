7898e17c7abddc4cee16502e729c6e1c7fdca6b2
Ticsup <contacto@ticsup.com>
2019-12-04 14:20:21 -0600

    ajax cubierta

--- grafos/urls.py
+++ grafos/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     path('coloreo/', views.ajax_coloreo, name='ajax_coloreo'),
     path('euler/', views.ajax_euler, name='ajax_euler'),
 
-    path('mis/', views.ajax_mis, name='ajax_mis'),
+    path('mis/', views.ajax_mis, name='ajax_mis')
+    path('cubierta/', views.ajax_cubierta, name='ajax_cubierta'),
 
     
     path('hami/', views.ajax_hamilton, name='ajax_hamilton'),
